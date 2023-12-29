464a9c7b20eea64d46bd71c2ebbfee1a30b44456
Cyri0 <nemestamas94@gmail.com>
2023-12-29 14:28:26 +0100

    keypresses works

--- public/index.js
+++ public/index.js
@@ -1,7 +1,13 @@
 const socket = io(`ws://localhost:5000`)
 
 const mapImage = new Image()
-mapImage.src = '/AllAssetsPreview.png'
+mapImage.src = '/assets/AllAssetsPreview.png'
+
+const skeletonImage = new Image()
+skeletonImage.src = '/assets/skeleton_front.png'
+
+const TILE_SIZE = 16
+const TILES_IN_ROW = 12
 
 const canvasEl = document.getElementById('canvas')
 canvasEl.width = window.innerWidth
@@ -11,9 +17,6 @@ const canvas = canvasEl.getContext("2d")
 
 let map = [[]]
 
-const TILE_SIZE = 16
-const TILES_IN_ROW = 12
-
 socket.on('connect', ()=>{
     console.log("connected")
 })
@@ -21,6 +24,48 @@ socket.on('connect', ()=>{
 socket.on('map', (loadedMap)=>{
     map = loadedMap
 })
+
+const inputs = {
+    'up': false,
+    'down': false,
+    'left': false,
+    'right': false,
+}
+
+window.addEventListener('keydown', (e)=>{
+    const k = e.key.toUpperCase();
+    if(k === 'W'){
+        inputs['up'] = true
+    }
+    else if(k === 'A'){
+        inputs['left'] = true
+    }
+    else if(k === 'S'){
+        inputs['down'] = true
+    }
+    else if(k === 'D'){
+        inputs['right'] = true
+    }
+    socket.emit('inputs', inputs)
+})
+
+window.addEventListener('keyup', (e)=>{
+    const k = e.key.toUpperCase();
+    if(k === 'W'){
+        inputs['up'] = false
+    }
+    else if(k === 'A'){
+        inputs['left'] = false
+    }
+    else if(k === 'S'){
+        inputs['down'] = false
+    }
+    else if(k === 'D'){
+        inputs['right'] = false
+    }
+    socket.emit('inputs', inputs)
+})
+
 
 function loop(){
     canvas.clearRect(0,0,canvas.width, canvas.height)
@@ -44,6 +89,8 @@ function loop(){
             )        
         }
     }
+
+    canvas.drawImage(skeletonImage,0,0)
 
     window.requestAnimationFrame(loop)
 }
